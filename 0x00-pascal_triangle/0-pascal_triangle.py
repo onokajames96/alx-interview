56d6611c71ebfd9d248a7b1198e1c5c0a68ac294
onokajames96 <jamesonoka996@gmail.com>
2024-03-07 00:25:04 -0800

    delete last commit

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -16,8 +16,8 @@ def pascal_triangle(n):
 
     for i in range(1, n):
         row = [1]
-        for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        for y in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][y])
         row.append(1)
         triangle.append(row)
 
