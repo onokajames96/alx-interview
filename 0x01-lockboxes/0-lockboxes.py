a0f222508d57672c3bb974cd37ee005df33a1fc2
onokajames96 <jamesonoka996@gmail.com>
2024-03-13 15:01:27 -0700

    alx interview

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+Defines a function that determines if all boxes can be opened
+"""
+
+
+def canUnlockAll(boxes):
+    """Determines if boxes can be opened"""
+    position = 0
+    unlocked = {}
+
+    for box in boxes:
+        if len(box) == 0 or position == 0:
+            unlocked[position] = "always_unlocked"
+        for key in box:
+            if key < len(boxes) and key != position:
+                unlocked[key] = key
+        if len(unlocked) == len(boxes):
+            return True
+        position += 1
+    return False
