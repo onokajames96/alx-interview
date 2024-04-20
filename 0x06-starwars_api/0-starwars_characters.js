44d59614952dded84a6902a6c9ecf9572e204e45
onokajames96 <jamesonoka996@gmail.com>
2024-04-20 06:37:40 -0700

    javacript and algorithms

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,14 +4,14 @@ const request = require('request');
 
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
   if (err) throw err;
-  const characterURLs = JSON.parse(body).characters;
-  exactOrder(characterURLs, 0);
+  const charURLs = JSON.parse(body).characters;
+  exactOrder(charURLs, 0);
 });
-const exactOrder = (characterURLs, x) => {
-  if (x === characterURLs.length) return;
-  request(characterURLs[x], function (err, res, body) {
+const exactOrder = (charURLs, x) => {
+  if (x === charURLs.length) return;
+  request(charURLs[x], function (err, res, body) {
     if (err) throw err;
     console.log(JSON.parse(body).name);
-    exactOrder(characterURLs, x + 1);
+    exactOrder(charURLs, x + 1);
   });
 };
